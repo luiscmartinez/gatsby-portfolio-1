eb62813e659d149a7c369294bcd37b4840acf219
Riley-Brown <riley@riley.gg>
2019-06-07 22:46:36 -0400

    import navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,12 +5,13 @@
  * See: https://www.gatsbyjs.org/docs/static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import React from 'react'
+import PropTypes from 'prop-types'
+import { StaticQuery, graphql } from 'gatsby'
+import Navbar from './Navbar'
 
-import Header from "./header"
-import "./layout.css"
+// import Header from "./header"
+import './layout.css'
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -25,22 +26,8 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
+        <Navbar />
+        <main>{children}</main>
       </>
     )}
   />
