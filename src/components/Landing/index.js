37daaaabc340473895a499c5e4bf8a2c79ceb3d4
Riley-Brown <riley@riley.gg>
2019-06-07 23:30:54 -0400

    created landing component

---
+++ src/components/Landing/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyledLanding } from './StyledLanding'
+
+export default function index() {
+  return (
+    <StyledLanding>
+      <h1>Hey</h1>
+    </StyledLanding>
+  )
+}
