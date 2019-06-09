523b565ce15e2a4b394cf8ba50d5fdd010920fe6
Riley-Brown <riley@riley.gg>
2019-06-08 23:56:03 -0400

    added landing content

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -4,7 +4,18 @@ import { StyledLanding } from './StyledLanding'
 export default function index() {
   return (
     <StyledLanding>
-      <h1>Hey</h1>
+      <div className="container">
+        <div>images will go here</div>
+        <div>
+          <h1>Welcome to my Portfolio</h1>
+          <p>
+            I am a Full-Stack Web Developer with a passion for learning new
+            technologies. I am experienced with modern tech such as React and
+            Nodejs.
+          </p>
+          <button>See More</button>
+        </div>
+      </div>
     </StyledLanding>
   )
 }
