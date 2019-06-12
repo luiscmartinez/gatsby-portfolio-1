0885c043cf6b148b98989994eb3fe95c345fa5ef
Riley Brown <riley@riley.gg>
2019-06-11 21:31:09 -0400

    created styles for carousel

---
+++ src/components/Carousel/StyledCarousel.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const StyledCarousel = styled.div`
+  .carousel-img {
+    position: absolute;
+    border: 2px solid #fff;
+    box-shadow: rgba(0, 0, 0, 0.4) 0px 6px 12px,
+      rgba(0, 0, 0, 0.23) 0px 6px 12px;
+  }
+`
