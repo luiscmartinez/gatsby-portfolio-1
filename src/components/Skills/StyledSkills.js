3e6b1a96bb4772fb0b473a6028539aeebb4b3b4a
Riley Brown <riley@riley.gg>
2019-06-13 22:42:30 -0400

    created skills section with styles

---
+++ src/components/Skills/StyledSkills.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const StyledSkills = styled.div`
+  padding: 50px 0;
+  h2 {
+    font-size: 5rem;
+    text-align: center;
+    color: var(--primary);
+  }
+  .styled-line {
+    width: 100px;
+    display: block;
+    margin: 20px auto;
+    border: 2px solid #222;
+  }
+`
---
+++ src/components/Skills/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { StyledSkills } from './StyledSkills'
+
+export default function index() {
+  return (
+    <StyledSkills>
+      <h2>My Skills</h2>
+      <hr className="styled-line" />
+    </StyledSkills>
+  )
+}
