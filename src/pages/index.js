af0ef85c918536f36711698c94c954ce3f0b4ba0
Riley Brown <riley@riley.gg>
2019-06-13 22:42:36 -0400

    import skills

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,12 @@ import Layout from '../components/layout'
 import Image from '../components/image'
 import SEO from '../components/seo'
 import Landing from '../components/Landing'
+import Skills from '../components/Skills'
 
 const IndexPage = () => (
   <Layout>
-    <Landing></Landing>
+    <Landing />
+    <Skills />
   </Layout>
 )
 
