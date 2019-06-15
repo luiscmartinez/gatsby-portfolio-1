f0dd39abfdac31e2258774c0491a78109d4e38ed
Riley Brown <riley@riley.gg>
2019-06-15 19:19:06 -0400

    created bounce animation keyframe

--- src/components/Testimonials/StyledTestimonials.js
+++ src/components/Testimonials/StyledTestimonials.js
@@ -58,6 +58,24 @@ export const StyledTestimonials = styled.div`
     .carousel-right {
       right: -120px;
       top: 50%;
+      animation: bounce 2s infinite;
+    }
+    @keyframes bounce {
+      0%,
+      20%,
+      50%,
+      80%,
+      100% {
+        transform: translateX(0);
+      }
+
+      40% {
+        transform: translateX(30px);
+      }
+
+      60% {
+        transform: translateX(15px);
+      }
     }
   }
 `
