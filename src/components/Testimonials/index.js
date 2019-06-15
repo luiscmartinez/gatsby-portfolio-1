4155d0eb01dbd238970f99f36fb544f4f40a9a32
Riley Brown <riley@riley.gg>
2019-06-15 18:46:19 -0400

    passed props

--- src/components/Testimonials/index.js
+++ src/components/Testimonials/index.js
@@ -5,8 +5,10 @@ import Testimonial from './Testimonial'
 export default function Testimonials() {
   return (
     <StyledTestimonials>
-      <h1>hey</h1>
-      <Testimonial title="cool title bro!" />
+      <Testimonial
+        title="Build personal website"
+        body="Great experience overall working with Riley. Very professional and quality of work exceeded my expectation. Will surely use again."
+      />
     </StyledTestimonials>
   )
 }
