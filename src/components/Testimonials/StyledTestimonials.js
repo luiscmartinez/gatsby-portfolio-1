9b85681f39dca013b059edf33bb297f68ec7eb38
Riley Brown <riley@riley.gg>
2019-06-15 18:45:18 -0400

    styled testimonial

--- src/components/Testimonials/StyledTestimonials.js
+++ src/components/Testimonials/StyledTestimonials.js
@@ -1,12 +1,35 @@
 import styled from 'styled-components'
 
 export const StyledTestimonials = styled.div`
-  padding: 50px 0;
+  padding-bottom: 50px;
   max-width: 700px;
   margin: auto;
   text-align: center;
+  width: 90%;
   .testimonial {
     h2 {
+      font-size: 3rem;
+      font-weight: normal;
+    }
+    .quote {
+      font-size: 5rem;
+      color: #333;
+      position: absolute;
+      top: -5px;
+      left: 10px;
+      margin-right: 5px;
+      display: inline-block;
+      height: 0;
+    }
+    p {
+      font-size: 2.1rem;
+      padding: 10px 20px;
+      line-height: 1.4;
+      position: relative;
+      opacity: 0.75;
+    }
+    .star {
+      color: var(--primary);
       font-size: 3rem;
     }
   }
