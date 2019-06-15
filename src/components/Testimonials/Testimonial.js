e4b256534ca9f45a8b7fd5db13ea396ed3ab33be
Riley Brown <riley@riley.gg>
2019-06-15 18:46:03 -0400

    added star symbols and body text prop

--- src/components/Testimonials/Testimonial.js
+++ src/components/Testimonials/Testimonial.js
@@ -4,6 +4,15 @@ export default function Testimonial(props) {
   return (
     <div className="testimonial">
       <h2>{props.title}</h2>
+      <span className="star">&#9733;</span>
+      <span className="star">&#9733;</span>
+      <span className="star">&#9733;</span>
+      <span className="star">&#9733;</span>
+      <span className="star">&#9733;</span>
+      <p>
+        <span className="quote">&#10077;</span>
+        {props.body}
+      </p>
     </div>
   )
 }
