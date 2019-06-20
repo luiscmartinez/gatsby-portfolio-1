de674b39b6592ca3d9544ed514ebc6ddf6ae105a
Riley Brown <riley@riley.gg>
2019-06-19 22:11:07 -0400

    added transition group wrapper

--- src/components/Testimonials/Testimonial.js
+++ src/components/Testimonials/Testimonial.js
@@ -1,18 +1,23 @@
 import React from 'react'
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 
 export default function Testimonial(props) {
   return (
-    <div className="testimonial">
-      <h2>{props.title}</h2>
-      <span className="star">&#9733;</span>
-      <span className="star">&#9733;</span>
-      <span className="star">&#9733;</span>
-      <span className="star">&#9733;</span>
-      <span className="star">&#9733;</span>
-      <div className="body">
-        <span className="quote">&#10077;</span>
-        <p>{props.body}</p>
-      </div>
-    </div>
+    <TransitionGroup>
+      <CSSTransition timeout={400} classNames="testimonial" key={props.index}>
+        <div className="testimonial">
+          <h2>{props.title}</h2>
+          <span className="star">&#9733;</span>
+          <span className="star">&#9733;</span>
+          <span className="star">&#9733;</span>
+          <span className="star">&#9733;</span>
+          <span className="star">&#9733;</span>
+          <div className="body">
+            <span className="quote">&#10077;</span>
+            <p>{props.body}</p>
+          </div>
+        </div>
+      </CSSTransition>
+    </TransitionGroup>
   )
 }
