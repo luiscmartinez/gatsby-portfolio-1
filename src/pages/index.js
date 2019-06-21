1c3e1456a17aa6c51ea5ccb60b7705080eaca3ed
Riley Brown <riley@riley.gg>
2019-06-20 22:03:40 -0400

    import work section

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,12 +7,14 @@ import SEO from '../components/seo'
 import Landing from '../components/Landing'
 import Skills from '../components/Skills'
 import Testimonials from '../components/Testimonials'
+import MyWork from '../components/MyWork'
 
 const IndexPage = () => (
   <Layout>
     <Landing />
     <Skills />
     <Testimonials />
+    <MyWork />
   </Layout>
 )
 
