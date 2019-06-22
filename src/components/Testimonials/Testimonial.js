55195654b3acac9abae057b431f8911804c4bb21
Riley Brown <riley@riley.gg>
2019-06-21 23:46:55 -0400

    added ref hook to get element height

--- src/components/Testimonials/Testimonial.js
+++ src/components/Testimonials/Testimonial.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import { TransitionGroup, CSSTransition } from 'react-transition-group'
 
 export default function Testimonial(props) {
+  const ref = useRef(null)
+  const [height, setHeight] = useState(0)
+
+  useEffect(() => {
+    console.log(ref.current.clientHeight)
+    console.log(height)
+    // setHeight(ref.current.clientHeight)
+  })
+
   return (
     <TransitionGroup>
       <CSSTransition timeout={400} classNames="testimonial" key={props.index}>
-        <div className="testimonial">
+        <div className="testimonial" ref={ref}>
           <h2>{props.title}</h2>
           <span className="star">&#9733;</span>
           <span className="star">&#9733;</span>
           <span className="star">&#9733;</span>
           <span className="star">&#9733;</span>
           <span className="star">&#9733;</span>
-          <div className="body">
+          <div
+            className="body"
+            style={{ transition: '300ms ease-out', height: `${height}px` }}
+          >
             <span className="quote">&#10077;</span>
             <p>{props.body}</p>
           </div>
