b03c8b4e9d9daf0513a36d4db66c52e8c37ea6a4
Riley Brown <riley@riley.gg>
2019-06-22 14:21:34 -0400

    removed unneeded ref and hooks

--- src/components/Testimonials/Testimonial.js
+++ src/components/Testimonials/Testimonial.js
@@ -1,30 +1,18 @@
-import React, { useRef, useEffect, useState } from 'react'
+import React from 'react'
 import { TransitionGroup, CSSTransition } from 'react-transition-group'
 
 export default function Testimonial(props) {
-  const ref = useRef(null)
-  const [height, setHeight] = useState(0)
-
-  useEffect(() => {
-    console.log(ref.current.clientHeight)
-    console.log(height)
-    // setHeight(ref.current.clientHeight)
-  })
-
   return (
     <TransitionGroup>
       <CSSTransition timeout={400} classNames="testimonial" key={props.index}>
-        <div className="testimonial" ref={ref}>
+        <div className="testimonial">
           <h2>{props.title}</h2>
           <span className="star">&#9733;</span>
           <span className="star">&#9733;</span>
           <span className="star">&#9733;</span>
           <span className="star">&#9733;</span>
           <span className="star">&#9733;</span>
-          <div
-            className="body"
-            style={{ transition: '300ms ease-out', height: `${height}px` }}
-          >
+          <div className="body">
             <span className="quote">&#10077;</span>
             <p>{props.body}</p>
           </div>
