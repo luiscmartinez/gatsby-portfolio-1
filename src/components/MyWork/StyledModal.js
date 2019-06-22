7b2f34b4f2cf434c6f5140e475391fc38ea11e01
Riley Brown <riley@riley.gg>
2019-06-22 14:59:58 -0400

    added styles for modal

--- src/components/MyWork/StyledModal.js
+++ src/components/MyWork/StyledModal.js
@@ -1,5 +1,39 @@
 import styled from 'styled-components'
 
 export const StyledModal = styled.div`
-  color: red;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.8);
+  width: 100%;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  z-index: 200;
+  display: none;
+  .container {
+    max-width: 1200px;
+    width: 90%;
+    margin: auto;
+    margin-top: 5%;
+    position: relative;
+    img {
+      width: 95%;
+    }
+    span {
+      position: absolute;
+      top: 0;
+      color: red;
+      font-size: 6rem;
+      right: 0px;
+      height: 0;
+      cursor: pointer;
+      opacity: 0.8;
+      transition: 200ms ease-out;
+      &:hover {
+        transition: 200ms ease-in;
+        opacity: 1;
+      }
+    }
+  }
 `
