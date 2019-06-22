4aa8ee901fd16895dfffb0237cbf7437024b9ff1
Riley Brown <riley@riley.gg>
2019-06-21 23:56:33 -0400

    removed ugly div soup for beautiful card component map

--- src/components/MyWork/index.js
+++ src/components/MyWork/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import bayshells from '../../images/bayshells-landing.png'
 import danielLanding from '../../images/daniel-landing-page.jpg'
 import instaClone from '../../images/insta-clone.png'
@@ -6,28 +6,23 @@ import learnLocker from '../../images/learn-locker.png'
 import caseStudy from '../../images/upwork-10-case-study.png'
 import wbFlashback from '../../images/wb-flashback.jpg'
 import { StyledMyWork } from './StyledMyWork'
+import Card from './Card'
 
 export default function MyWork() {
+  const [imgs] = useState([
+    learnLocker,
+    caseStudy,
+    instaClone,
+    danielLanding,
+    wbFlashback,
+    bayshells,
+  ])
+
   return (
     <StyledMyWork>
-      <div className="card">
-        <img src={learnLocker} alt="" />
-      </div>
-      <div className="card">
-        <img src={caseStudy} alt="" />
-      </div>
-      <div className="card">
-        <img src={instaClone} alt="" />
-      </div>
-      <div className="card">
-        <img src={danielLanding} alt="" />
-      </div>
-      <div className="card">
-        <img src={wbFlashback} alt="" />
-      </div>
-      <div className="card">
-        <img src={bayshells} alt="" />
-      </div>
+      {imgs.map(img => (
+        <Card img={img} />
+      ))}
     </StyledMyWork>
   )
 }
