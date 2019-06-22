a19bfc8cb8558326edae90a40e1b2c041bf4c13b
Riley Brown <riley@riley.gg>
2019-06-22 15:03:09 -0400

    added toggle modal onClick to span

--- src/components/MyWork/Modal.js
+++ src/components/MyWork/Modal.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { StyledModal } from './StyledModal'
 
-export default function Modal({ img, show }) {
+export default function Modal({ img, show, toggleModal }) {
   return (
     <StyledModal style={{ display: show ? 'block' : 'none' }}>
       <div className="container">
         <img src={img} alt="" />
-        <span>&times;</span>
+        <span onClick={toggleModal}>&times;</span>
       </div>
     </StyledModal>
   )
