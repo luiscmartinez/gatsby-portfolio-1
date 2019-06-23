373df9d2f759428705e3afba43c654dc655f267a
Riley Brown <riley@riley.gg>
2019-06-22 22:46:56 -0400

    wrapped modal in transition group

--- src/components/MyWork/Modal.js
+++ src/components/MyWork/Modal.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { StyledModal } from './StyledModal'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 export default function Modal({ img, show, toggleModal }) {
   return (
-    <StyledModal style={{ display: show ? 'block' : 'none' }}>
-      <div className="container">
-        <img src={img} alt="" />
-        <span onClick={toggleModal}>&times;</span>
-      </div>
-    </StyledModal>
+    <TransitionGroup>
+      <CSSTransition timeout={500} classNames="modal" key={img} appear={true}>
+        <StyledModal>
+          <div className="container">
+            <img src={img} alt="" />
+            <span onClick={toggleModal}>&times;</span>
+          </div>
+        </StyledModal>
+      </CSSTransition>
+    </TransitionGroup>
   )
 }
