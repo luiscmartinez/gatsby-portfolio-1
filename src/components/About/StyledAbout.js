767f25c47869cde2ca2338cf19b6b206bae11fc9
Riley Brown <riley@riley.gg>
2019-06-22 20:39:26 -0400

    Created about/contact section with styles

---
+++ src/components/About/StyledAbout.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components'
+
+export const StyledAbout = styled.div`
+  .container {
+    padding: 100px 0;
+    max-width: 1400px;
+    margin: auto;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    .about,
+    .contact {
+      flex: 0 0 45%;
+    }
+    .contact {
+      form {
+        display: flex;
+        flex-direction: column;
+        input,
+        textarea {
+          padding: 10px;
+          border: 1px solid lightgrey;
+          border-radius: 5px;
+          margin-bottom: 10px;
+          resize: none;
+          font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        }
+        #submit {
+          background: var(--primary);
+          color: #fff;
+          font-size: 1.6rem;
+        }
+      }
+    }
+  }
+  h2 {
+    font-size: 4rem;
+    color: #222;
+    text-align: center;
+    margin-bottom: 10px;
+  }
+  p {
+    font-size: 1.9rem;
+    margin-bottom: 20px;
+    line-height: 1.6;
+  }
+`
---
+++ src/components/About/index.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { StyledAbout } from './StyledAbout'
+export default function index() {
+  return (
+    <StyledAbout>
+      <div className="container">
+        <div className="about">
+          <h2>About Me</h2>
+          <p>
+            I am a 23 year old developer with a focus on web technology, from
+            Boston, Massachusetts. I discovered web development in early 2018
+            and have been fascinated with it ever since
+          </p>
+          <p>
+            I started with HTML on CodeCademy, then CSS and JavaScript. I moved
+            on to Team TreeHouse's 80 hour front-end development course, as well
+            as many YouTube and Udemy courses. I couldn't get enough, I was
+            hooked.
+          </p>
+          <p>
+            I started Lambda School's 9 month coding school in late August 2018,
+            and learned valuable skills such as React, Node.js, SQL, Python and
+            C. Most importantly, I learned how to learn.
+          </p>
+          <p>
+            I am currently working as a front-end developer for an awesome
+            startup in NYC and am loving every second of it.
+          </p>
+        </div>
+        <div className="contact">
+          <h2>Contact Me</h2>
+          <form>
+            <input type="text" placeholder="Name" />
+            <input type="email" placeholder="Email" />
+            <textarea name="" placeholder="Message" id="" cols="30" rows="10" />
+            <input
+              type="submit"
+              value="Submit Form"
+              id="submit"
+              name="submit"
+            />
+          </form>
+        </div>
+      </div>
+    </StyledAbout>
+  )
+}
