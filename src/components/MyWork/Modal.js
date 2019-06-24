6c0e5414d63e435ee8f8e88d8187fba021a81e9b
Riley Brown <riley@riley.gg>
2019-06-23 23:06:23 -0400

    added in and unmount prop to fix exit transition

--- src/components/MyWork/Modal.js
+++ src/components/MyWork/Modal.js
@@ -4,15 +4,13 @@ import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 export default function Modal({ img, show, toggleModal }) {
   return (
-    <TransitionGroup>
-      <CSSTransition timeout={500} classNames="modal" key={img} appear={true}>
-        <StyledModal>
-          <div className="container">
-            <img src={img} alt="" />
-            <span onClick={toggleModal}>&times;</span>
-          </div>
-        </StyledModal>
-      </CSSTransition>
-    </TransitionGroup>
+    <CSSTransition timeout={300} classNames="modal" in={show} unmountOnExit>
+      <StyledModal>
+        <div className="container">
+          <img src={img} alt="" />
+          <span onClick={toggleModal}>&times;</span>
+        </div>
+      </StyledModal>
+    </CSSTransition>
   )
 }
