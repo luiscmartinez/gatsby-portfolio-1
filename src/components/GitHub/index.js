c418104e9c5f01f692f96248387c733b7b61142d
Riley Brown <riley@riley.gg>
2019-06-26 23:31:30 -0400

    added functionality to set latest repos in state with hooks

--- src/components/GitHub/index.js
+++ src/components/GitHub/index.js
@@ -1,6 +1,27 @@
-import React from 'react'
-import StyledGitHub from './StyledGitHub'
+import React, { useState, useEffect } from 'react'
+import { StyledGitHub } from './StyledGitHub'
+import axios from 'axios'
 
-export default function index() {
-  return <div>Heyooo</div>
+export default function GitHub() {
+  const [latestRepos, setLatestRepos] = useState([])
+
+  useEffect(() => {
+    // setLatestRepos(repos)
+    const repos = async () => {
+      const data = await axios.get(
+        'https://supersystem-mailchimp-api.herokuapp.com/github'
+      )
+      console.log(data)
+      setLatestRepos(data.data)
+    }
+    repos()
+  }, [])
+
+  return (
+    <StyledGitHub>
+      {latestRepos.map(repo => (
+        <h1>{repo.name}</h1>
+      ))}
+    </StyledGitHub>
+  )
 }
