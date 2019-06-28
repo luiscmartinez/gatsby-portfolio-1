2b5968f03c6202b2b20fcdb9373766746cb929b0
Riley Brown <riley@riley.gg>
2019-06-27 22:17:34 -0400

    added fade in for testimonial

--- src/components/Testimonials/Testimonial.js
+++ src/components/Testimonials/Testimonial.js
@@ -1,23 +1,26 @@
 import React from 'react'
 import { TransitionGroup, CSSTransition } from 'react-transition-group'
+import Fade from 'react-reveal/Fade'
 
 export default function Testimonial(props) {
   return (
-    <TransitionGroup>
-      <CSSTransition timeout={400} classNames="testimonial" key={props.index}>
-        <div className="testimonial">
-          <h2>{props.title}</h2>
-          <span className="star">&#9733;</span>
-          <span className="star">&#9733;</span>
-          <span className="star">&#9733;</span>
-          <span className="star">&#9733;</span>
-          <span className="star">&#9733;</span>
-          <div className="body">
-            <span className="quote">&#10077;</span>
-            <p>{props.body}</p>
+    <Fade>
+      <TransitionGroup>
+        <CSSTransition timeout={400} classNames="testimonial" key={props.index}>
+          <div className="testimonial">
+            <h2>{props.title}</h2>
+            <span className="star">&#9733;</span>
+            <span className="star">&#9733;</span>
+            <span className="star">&#9733;</span>
+            <span className="star">&#9733;</span>
+            <span className="star">&#9733;</span>
+            <div className="body">
+              <span className="quote">&#10077;</span>
+              <p>{props.body}</p>
+            </div>
           </div>
-        </div>
-      </CSSTransition>
-    </TransitionGroup>
+        </CSSTransition>
+      </TransitionGroup>
+    </Fade>
   )
 }
