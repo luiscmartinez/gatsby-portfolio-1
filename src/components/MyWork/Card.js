c9a375ab54c316b0559e62766b80b4cc4352e5a1
Riley Brown <riley@riley.gg>
2019-06-27 22:16:19 -0400

    added fade in effect

--- src/components/MyWork/Card.js
+++ src/components/MyWork/Card.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import Fade from 'react-reveal/Fade'
 
 export default function Card({ img, activeModalImg }) {
+  const key = Math.random()
   return (
-    <div className="card">
-      <img
-        src={img}
-        alt="my super cool projects... very cool... cool stuff"
-        onClick={() => activeModalImg(img)}
-      />
-    </div>
+    <Fade key={key}>
+      <div className="card">
+        <img
+          src={img}
+          alt="my super cool projects... very cool... cool stuff"
+          onClick={() => activeModalImg(img)}
+        />
+      </div>
+    </Fade>
   )
 }
