c1b435f6958aceadcf4478241fb7e80aef2843c9
Riley Brown <riley@riley.gg>
2019-06-29 17:02:39 -0400

    adjusted props, added content div

--- src/components/MyWork/Modal.js
+++ src/components/MyWork/Modal.js
@@ -2,16 +2,30 @@ import React from 'react'
 import { StyledModal } from './StyledModal'
 import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
-export default function Modal({ img, show, toggleModal }) {
+export default function Modal(props) {
+  const { title, body, siteLink, repoLink, img } = props.data
   return (
-    <CSSTransition timeout={300} classNames="modal" in={show} unmountOnExit>
+    <CSSTransition
+      timeout={300}
+      classNames="modal"
+      in={props.show}
+      unmountOnExit
+    >
       <StyledModal
-        onClick={e => (e.target.id === 'modal-backdrop' ? toggleModal() : null)}
+        onClick={e =>
+          e.target.id === 'modal-backdrop' ? props.toggleModal() : null
+        }
         id="modal-backdrop"
       >
         <div className="container">
           <img src={img} alt="" />
-          <span onClick={toggleModal}>&times;</span>
+          <span onClick={null}>&times;</span>
+          <div className="modal content">
+            <h4>{title}</h4>
+            <p>{body}</p>
+            <a href={siteLink}>Visit Site</a>
+            <a href={repoLink}>View Code</a>
+          </div>
         </div>
       </StyledModal>
     </CSSTransition>
