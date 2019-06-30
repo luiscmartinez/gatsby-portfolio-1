b7b7ec99daf3a00738ba7fd08cf2e10a87c8dd76
Riley Brown <riley@riley.gg>
2019-06-30 16:11:01 -0400

    media queries for skills section

--- src/components/Skills/StyledSkills.js
+++ src/components/Skills/StyledSkills.js
@@ -6,6 +6,9 @@ export const StyledSkills = styled.div`
     font-size: 5rem;
     text-align: center;
     color: var(--primary);
+    @media (max-width: 500px) {
+      font-size: 4rem;
+    }
   }
   .styled-line {
     width: 100px;
@@ -20,6 +23,11 @@ export const StyledSkills = styled.div`
     width: 90%;
     max-width: 1200px;
     margin: 40px auto;
+    @media (max-width: 900px) {
+      :last-of-type {
+        margin: 0 auto;
+      }
+    }
     .card {
       flex: 0 0 30%;
       display: flex;
@@ -31,6 +39,12 @@ export const StyledSkills = styled.div`
       background: #fff;
       padding: 20px;
       box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 1px 6px rgba(0, 0, 0, 0.23);
+      @media (max-width: 768px) {
+        flex: 0 0 45%;
+      }
+      @media (max-width: 500px) {
+        flex: 0 0 100%;
+      }
       h1 {
         text-align: center;
         font-size: 2.3rem;
