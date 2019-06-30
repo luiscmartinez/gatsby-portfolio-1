98d84c97a2319968ff04c69921c7e68d08733eab
Riley Brown <riley@riley.gg>
2019-06-30 14:30:10 -0400

    imported lazy load package and wrapped each img

--- src/components/MyWork/Card.js
+++ src/components/MyWork/Card.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import Fade from 'react-reveal/Fade'
+import LazyLoad from 'react-lazy-load'
 
 export default function Card({ img, activeModalImg, index }) {
   const key = Math.random()
   return (
     <Fade key={key}>
       <div className="card">
-        <img
-          src={img}
-          alt="my super cool projects... very cool... cool stuff"
-          onClick={() => activeModalImg(index)}
-        />
+        <LazyLoad offsetVertical={500}>
+          <img
+            src={img}
+            alt="my super cool projects... very cool... cool stuff"
+            onClick={() => activeModalImg(index)}
+          />
+        </LazyLoad>
       </div>
     </Fade>
   )
