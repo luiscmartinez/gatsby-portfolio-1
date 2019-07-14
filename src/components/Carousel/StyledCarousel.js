23538defa39d508f3d89455a3fab1722b93e3771
Riley Brown <riley@riley.gg>
2019-07-14 00:09:05 -0400

    adjusted carousel styles

--- src/components/Carousel/StyledCarousel.js
+++ src/components/Carousel/StyledCarousel.js
@@ -4,5 +4,10 @@ export const StyledCarousel = styled.div`
   .carousel-img {
     position: absolute;
     border: 2px solid #fff;
+    height: 100%;
+    max-height: 330px;
+    @media (max-width: 1230px) {
+      /* max-height: initial; */
+    }
   }
 `
