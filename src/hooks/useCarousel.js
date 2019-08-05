fb7673b7ebdf161ad6de9ea2f9cf2c0e11f003e2
Riley Brown <riley@riley.gg>
2019-08-04 23:50:24 -0400

    added functions to programmatically control carousel

--- src/hooks/useCarousel.js
+++ src/hooks/useCarousel.js
@@ -1,27 +1,50 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useIsVisible } from './useIsVisible'
 
-export function useCarousel(data, time, element) {
+export function useCarousel({
+  data,
+  timeout,
+  element,
+  defaultVisible,
+  arrowKeysCycle,
+}) {
   const [index, setIndex] = useState(0)
-  const visible = useIsVisible(element)
+  const visible = useIsVisible({ element, defaultVisible })
+  const [int, setInt] = useState(null)
+  const [paused, setPaused] = useState(false)
 
   useEffect(() => {
     let interval
 
     // start interval if visible, else clear interval
     if (visible) {
-      interval = setInterval(increment, time)
+      interval = setInterval(increment, timeout)
+      setInt(interval)
+      // cycle carousel with arrow keys
+      arrowKeysCycle && window.addEventListener('keydown', arrowKeys)
     } else {
       clearInterval(interval)
+      setInt(null)
+      arrowKeysCycle && window.removeEventListener('keydown', arrowKeys)
     }
 
     return () => {
       clearInterval(interval)
+      window.removeEventListener('keydown', arrowKeys)
     }
-  }, [index, visible])
+  }, [index, visible, paused])
 
   // increment index, reset to 0 when index is on last item
   const increment = () => {
+    if (!paused && visible)
+      if (index >= data.length - 1) {
+        setIndex(0)
+      } else {
+        setIndex(index => index + 1)
+      }
+  }
+
+  const next = () => {
     if (index >= data.length - 1) {
       setIndex(0)
     } else {
@@ -29,5 +52,36 @@ export function useCarousel(data, time, element) {
     }
   }
 
-  return [index]
+  const previous = () => {
+    if (index === 0) {
+      setIndex(data.length - 1)
+    } else {
+      setIndex(index => index - 1)
+    }
+  }
+
+  const indexClick = i => setIndex(i)
+
+  const pause = () => {
+    setPaused(true)
+    clearInterval(int)
+    setInt(null)
+  }
+
+  const resume = () => {
+    setPaused(false)
+    let interval = setInterval(increment, timeout)
+    setInt(interval)
+  }
+
+  // cycle carousel with arrow keys
+  const arrowKeys = e => {
+    if (e.key === 'ArrowRight' || e.keyCode === 39) {
+      next()
+    } else if (e.key === 'ArrowLeft' || e.keyCode === 37) {
+      previous()
+    }
+  }
+
+  return [index, next, previous, indexClick, pause, resume]
 }
