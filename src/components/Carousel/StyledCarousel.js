ab9b9ad9c97a4ce85a36b6a4e660d3d05b84cbc0
Riley Brown <riley@riley.gg>
2019-11-24 15:11:14 -0500

    added img wrapper style to fix gatsby img positioning

--- src/components/Carousel/StyledCarousel.js
+++ src/components/Carousel/StyledCarousel.js
@@ -1,7 +1,15 @@
 import styled from 'styled-components'
 
 export const StyledCarousel = styled.div`
-  .carousel-img {
+  .img-wrapper {
+    position: absolute;
+    top: 70px;
+    right: 0;
+    left: 0;
+    bottom: 0;
+  }
+  .carousel-img,
+  .gatsby-image-wrapper {
     position: absolute;
     border: 2px solid #fff;
     height: 100%;
