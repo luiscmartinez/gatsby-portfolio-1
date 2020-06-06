1674d439b381ec4f112e151f17070ae19c838694
Riley Brown <riley@riley.gg>
2020-06-06 19:52:41 -0400

    moved MyWork data array outside of component, adjusted styles

--- src/components/MyWork/StyledMyWork.js
+++ src/components/MyWork/StyledMyWork.js
@@ -3,7 +3,6 @@ import { transitionClasses } from '../../helpers/transitionClasses'
 
 export const StyledMyWork = styled.div`
   ${transitionClasses('modal', 300)};
-  margin-top: 20px;
   .cards {
     width: 90%;
     max-width: 1200px;
@@ -16,8 +15,6 @@ export const StyledMyWork = styled.div`
     height: 220px;
     margin: auto;
     margin-bottom: 40px;
-    box-shadow: rgba(0, 0, 0, 0.4) 0px 6px 12px,
-      rgba(0, 0, 0, 0.23) 0px 6px 12px;
     box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 1px 6px rgba(0, 0, 0, 0.23);
     border-radius: 12px;
     @media (max-width: 1300px) {
@@ -32,7 +29,6 @@ export const StyledMyWork = styled.div`
       width: 100%;
       object-fit: fill;
       height: 220px;
-      border: 1px solid #444;
       border-radius: 12px;
       cursor: pointer;
       display: block;
--- src/components/MyWork/index.js
+++ src/components/MyWork/index.js
@@ -11,58 +11,58 @@ import learnLocker from 'images/learn-locker.png'
 import caseStudy from 'images/upwork-10-case-study.jpg'
 import wbFlashback from 'images/wb-flashback.jpg'
 
-export default function MyWork() {
-  const data = [
-    {
-      img: learnLocker,
-      title: 'Learn Locker',
-      body:
-        'LearnLocker is a full-stack social network learning application, built with 4 other web developers. Built using React, Node.js and PostgreSQL.',
-      siteLink: 'https://learnedadev.netlify.com/',
-      codeLink: 'https://github.com/learneda',
-    },
-    {
-      img: caseStudy,
-      title: 'UpWork Freelance Work',
-      body:
-        'Website for a client featuring custom hover animations, and custom image carousel using HTML 5 data attributes. Design provided by client.',
-      siteLink: 'https://darren.riley.gg/case-study',
-      codeLink: 'https://github.com/Riley-Brown/upwork-10',
-    },
-    {
-      img: rawberri,
-      title: 'Rawberri Order Builder',
-      body:
-        'Freelance work developing online ordering platform for restaurant. Uses AWS lambda functions for ordering confirmations.',
-      siteLink: 'https://loving-kilby-fc3f0a.netlify.com/',
-      codeLink: 'https://github.com/Riley-Brown/gatsby-online-food-orders',
-    },
-    {
-      img: danielLanding,
-      title: 'Daniel D Website',
-      body:
-        'Personal website built for a client. The awesome design was provided by the client, I just put it all together. Built using Bootstrap.',
-      codeLink: 'https://github.com/Riley-Brown/upwork-4',
-      siteLink: 'https://daniel.riley.gg/',
-    },
-    {
-      img: wbFlashback,
-      title: 'WB Flashback',
-      body:
-        'Website built to showcase the changes in Brooklyn, New York, over many years. Includes amazing cross-fade, and split image before and after effects. Built using CSS, and JavaScript.',
-      siteLink: 'https://wb-flashback.riley.gg/',
-      codeLink: 'https://github.com/Riley-Brown/upwork-3',
-    },
-    {
-      img: bayshells,
-      title: 'Bay Shells Landing Page',
-      body:
-        'Beautiful modern design provided by the client, includes dynamic clickable tabs, and slick carousel. Built using vanilla CSS and jQuery.',
-      siteLink: 'https://bayshells.riley.gg/',
-      codeLink: 'https://github.com/Riley-Brown/bayshells-landing',
-    },
-  ]
+const data = [
+  {
+    img: learnLocker,
+    title: 'Learn Locker',
+    body:
+      'LearnLocker is a full-stack social network learning application, built with 4 other web developers. Built using React, Node.js and PostgreSQL.',
+    siteLink: 'https://learnedadev.netlify.com/',
+    codeLink: 'https://github.com/learneda',
+  },
+  {
+    img: caseStudy,
+    title: 'UpWork Freelance Work',
+    body:
+      'Website for a client featuring custom hover animations, and custom image carousel using HTML 5 data attributes. Design provided by client.',
+    siteLink: 'https://darren.riley.gg/case-study',
+    codeLink: 'https://github.com/Riley-Brown/upwork-10',
+  },
+  {
+    img: rawberri,
+    title: 'Rawberri Order Builder',
+    body:
+      'Freelance work developing online ordering platform for restaurant. Uses AWS lambda functions for ordering confirmations.',
+    siteLink: 'https://loving-kilby-fc3f0a.netlify.com/',
+    codeLink: 'https://github.com/Riley-Brown/gatsby-online-food-orders',
+  },
+  {
+    img: danielLanding,
+    title: 'Daniel D Website',
+    body:
+      'Personal website built for a client. The awesome design was provided by the client, I just put it all together. Built using Bootstrap.',
+    codeLink: 'https://github.com/Riley-Brown/upwork-4',
+    siteLink: 'https://daniel.riley.gg/',
+  },
+  {
+    img: wbFlashback,
+    title: 'WB Flashback',
+    body:
+      'Website built to showcase the changes in Brooklyn, New York, over many years. Includes amazing cross-fade, and split image before and after effects. Built using CSS, and JavaScript.',
+    siteLink: 'https://wb-flashback.riley.gg/',
+    codeLink: 'https://github.com/Riley-Brown/upwork-3',
+  },
+  {
+    img: bayshells,
+    title: 'Bay Shells Landing Page',
+    body:
+      'Beautiful modern design provided by the client, includes dynamic clickable tabs, and slick carousel. Built using vanilla CSS and jQuery.',
+    siteLink: 'https://bayshells.riley.gg/',
+    codeLink: 'https://github.com/Riley-Brown/bayshells-landing',
+  },
+]
 
+export default function MyWork() {
   const [show, setShow] = useState(false)
   const [index, setIndex] = useState(0)
 
