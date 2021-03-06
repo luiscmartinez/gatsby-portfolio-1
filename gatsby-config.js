e50e2487a6e606f9d302dd1e0c624705f585f4c6
Riley <riley@riley.gg>
2021-03-06 18:34:01 -0500

    upgrade to gatsby v3

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,5 +31,7 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-image`,
+    'gatsby-plugin-root-import',
   ],
 }
