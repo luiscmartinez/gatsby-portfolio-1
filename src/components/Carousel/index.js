4b0639e3d65f608b5d5ec37ff2b56d6087200e1a
Luis Martinez <mrluismartinezzz@gmail.com>
2021-07-22 15:49:57 -0700

    updated Carousel.js
    using graphql to get imgs

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,21 +1,20 @@
 import React, { useRef } from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
-import { GatsbyImage } from 'gatsby-plugin-image'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
 import { TransitionGroup, CSSTransition } from 'react-transition-group'
 import { StyledCarousel } from './StyledCarousel'
 import { useCarousel } from 'hooks/useCarousel'
 
 export default function Carousel() {
+  
   const imageData = useStaticQuery(graphql`
     query LandingCarouselQuery {
-      allImageSharp {
+      allFile(filter: { sourceInstanceName: { eq: "carouselImages" } }) {
         edges {
           node {
-            gatsbyImageData(placeholder: BLURRED, height: 330)
-            fluid {
-              ...GatsbyImageSharpFluid
-              originalName
+            childImageSharp {
+              gatsbyImageData
             }
           }
         }
@@ -23,26 +22,12 @@ export default function Carousel() {
     }
   `)
 
-  const imageNames = [
-    'datatonic-2.png',
-    'pepe-mexican-food-2.png',
-    'learn-locker.png',
-    'learn-locker-logged-in.png',
-  ]
-
-  const images = imageData.allImageSharp.edges
-    .filter(edge => {
-      for (let i = 0; i < imageNames.length; i++) {
-        if (edge.node.fluid.originalName === imageNames[i]) {
-          return edge
-        }
-      }
-    })
-    .reverse()
+  const carouselImages = imageData.allFile.edges.map(edge => getImage(edge.node))
 
   const carouselContainer = useRef()
+
   const [index] = useCarousel({
-    data: images,
+    data: carouselImages,
     timeout: 2000,
     element: carouselContainer,
     defaultVisible: true,
@@ -58,7 +43,7 @@ export default function Carousel() {
           appear={true}
         >
           <div className="img-wrapper">
-            <GatsbyImage image={images[index].node.gatsbyImageData} />
+            <GatsbyImage image={carouselImages[index]} alt="carousel site img" />
           </div>
         </CSSTransition>
       </TransitionGroup>
