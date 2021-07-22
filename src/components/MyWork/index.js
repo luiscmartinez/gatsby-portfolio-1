0dffb37bb38f24f3763bc5556de25f9404f5e4c3
Luis Martinez <mrluismartinezzz@gmail.com>
2021-07-22 15:48:19 -0700

    updated Mywork/index.js
    querying data with graphql

--- src/components/MyWork/index.js
+++ src/components/MyWork/index.js
@@ -4,68 +4,60 @@ import { StyledMyWork } from './StyledMyWork'
 import Card from './Card'
 import Modal from './Modal'
 
-import learnLocker from 'images/learn-locker.png'
-import pepesMexicanFoodImg from 'images/pepe-mexican-food.png'
-import dataTonicImg from 'images/datatonic.png'
+import { graphql, useStaticQuery } from 'gatsby'
 
-const data = [
-  {
-    img: learnLocker,
-    title: 'Learn Locker',
-    body:
-      'LearnLocker is a full-stack social network learning application, built with 4 other web developers. Built using React, Node.js and PostgreSQL.',
-    siteLink: 'https://learnedadev.netlify.com/',
-    codeLink: 'https://github.com/learneda',
-  },
-  {
-    img: pepesMexicanFoodImg,
-    title: "Original Pepe's Mexican Food ",
-    body:
-      "Refactor client's multiple html page website into a single page application.",
-    siteLink: 'https://original-pepes.luismartinez.dev',
-    codeLink: 'https://github.com/luiscmartinez/originalpepes',
-  },
-  {
-    img: dataTonicImg,
-    title: 'Data Tonic',
-    body: 'A Take-Home Project assignment for Prism Studios.',
-    siteLink: 'https://datatonic.luismartinez.dev/dashboard',
-    codeLink: 'https://github.com/luiscmartinez/datatonic-fe',
-  },
-]
+
 
 export default function MyWork() {
   const [show, setShow] = useState(false)
   const [clickedIndex, setIndex] = useState(0)
+
+  const myWorkQuey = useStaticQuery(graphql`
+  query MyWorkQuey {
+    allJson {
+      edges {
+        node {
+          id
+          body
+          title
+          siteLink
+          img
+        }
+      }
+    }
+  }
+  `)
+
+  const toggleModal = () => setShow(!show)
 
   const activeModalImg = index => {
     setIndex(index)
     toggleModal()
   }
 
-  const toggleModal = () => setShow(!show)
-
+  const myWorkList = myWorkQuey.allJson.edges.map(edge => edge.node)
+  
   return (
     <StyledMyWork>
       <div className="cards">
-        {data.map((item, index) => (
+        {myWorkList.map((item, index) => (
           <Card
             activeModalImg={activeModalImg}
-            key={item.img}
-            img={item.img}
+            key={item.id}
             index={index}
             title={item.title}
           />
         ))}
       </div>
       <Modal
-        data={data[clickedIndex]}
+        data={myWorkList[clickedIndex]}
         setIndex={setIndex}
         toggleModal={toggleModal}
         show={show}
         clickedIndex={clickedIndex}
-        dataLength={data.length}
+        dataLength={myWorkList.length}
       />
     </StyledMyWork>
   )
 }
+
